7c1ec21775fca1595340e7fd5f4c22bfc1beed77
Rui Craveiro <ruicraveiro@squarealfa.com>
2022-04-09 04:16:12 +0100

    Fixed missing exports

--- grpc_host/CHANGELOG.md
+++ grpc_host/CHANGELOG.md
@@ -1,4 +1,7 @@
-## 3.0.9
+## 3.1.1
+- Fixed missing exports
+
+## 3.1.0
 - Removed deprecated smtp configuration support.
 - Supporting classless hosts
 
--- grpc_host/lib/src/hosting/hosting.dart
+++ grpc_host/lib/src/hosting/hosting.dart
@@ -2,3 +2,5 @@ export 'services_host.dart';
 export 'principal_interceptor.dart';
 export 'host_parameters.dart';
 export 'host.dart';
+export 'isolates.dart';
+export 'tls.dart';
