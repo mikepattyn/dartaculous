dfa61f66ba733ae356c6ab640547c55d529a426a
Rui Craveiro <ruicraveiro@squarealfa.com>
2022-05-03 23:11:25 +0100

    feat: CrudServiceMethods mixin supports transactions

--- grpc_host/lib/src/services/crud_service_methods.dart
+++ grpc_host/lib/src/services/crud_service_methods.dart
@@ -4,31 +4,48 @@ import 'package:nosql_repository/nosql_repository.dart';
 import 'package:squarealfa_security/squarealfa_security.dart';
 
 mixin CrudServiceMethods<TEntity extends Object> on EntityServices<TEntity> {
-  Future<TEntity> create(TEntity entity) async {
+  Future<TEntity> create(
+    TEntity entity, {
+    RepositoryTransaction? transaction,
+  }) async {
     validator.validateThrowing(entity);
     var map = mapMapper.toMap(entity);
-    map = await repository.create(
-      map,
-      call.principal,
-    );
+    map =
+        await repository.create(map, call.principal, transaction: transaction);
 
     entity = mapMapper.fromMap(map);
     return entity;
   }
 
-  Future deleteByKeyValue(String keyValue) async {
+  Future deleteByKeyValue(
+    String keyValue, {
+    RepositoryTransaction? transaction,
+  }) async {
     await repository.delete(
       keyValue,
       principal,
+      transaction: transaction,
     );
   }
 
-  Future<TEntity> getByKeyValue(String keyValue) async {
-    var entity = await _get(principal, keyValue);
+  Future<TEntity> getByKeyValue(
+    String keyValue, {
+    RepositoryTransaction? transaction,
+  }) async {
+    var map = await repository.get(
+      keyValue,
+      principal,
+      transaction: transaction,
+    );
+    var entity = mapMapper.fromMap(map);
+
     return entity;
   }
 
-  Future<TEntity> update(TEntity entity) async {
+  Future<TEntity> update(
+    TEntity entity, {
+    RepositoryTransaction? transaction,
+  }) async {
     validator.validateThrowing(entity);
 
     var map = mapMapper.toMap(entity);
@@ -37,6 +54,7 @@ mixin CrudServiceMethods<TEntity extends Object> on EntityServices<TEntity> {
       map = await repository.update(
         map,
         principal,
+        transaction: transaction,
       );
     } on NotFound {
       throw GrpcError.notFound();
@@ -44,13 +62,6 @@ mixin CrudServiceMethods<TEntity extends Object> on EntityServices<TEntity> {
       throwUnauthorized();
     }
     entity = mapMapper.fromMap(map);
-
-    return entity;
-  }
-
-  Future<TEntity> _get(Principal principal, String key) async {
-    var map = await repository.get(key, principal);
-    var entity = mapMapper.fromMap(map);
 
     return entity;
   }
--- grpc_host/lib/src/services/entity_services.dart
+++ grpc_host/lib/src/services/entity_services.dart
@@ -72,4 +72,6 @@ class EntityServices<TEntity> extends AuthenticatedServices {
     );
     return ret;
   }
+
+  
 }
